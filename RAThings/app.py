32cbcba5aae94fcdaa656c44d9a155d9eee143fb
amlgnc <alex2002hiphop@gmail.com>
2023-02-13 20:51:39 +0100

    Update console application

--- RAThings/app.py
+++ RAThings/app.py
@@ -1,20 +1,52 @@
 import json
+import webbrowser
+
+
+def printHelp():
+    print("")
+    print("exit      : exit the console application                    :")
+    print("help      : show help                                       :")
+    print("list      : show 'id-list.json'                             :")
+    print("check     : open the jr. developer review requests web page :")
+    print("game <id> : open the game web page                          :")
+    print("          : <id> : the retroachievements.org game id        :")
 
 
 def printList():
     f = open('RAThings/id-list.json')
     data = json.load(f)
-    print(f'{data}\n')
+    print(f'{data}')
     f.close()
+
+
+def openChecklistWebPage():
+    webbrowser.open("https://docs.google.com/spreadsheets/d/e/2PACX-1vSl4tRG-wV1lxtb-2ZJYRoM0VLnQPYoQO1jOVRmb8TFWldFJGr7RrO6-I-c6rWK0XsZ1h5pJEOStjmQ/pubhtml?gid=1582422742&single=true")
+
+
+def openGameWebPage(cmd):
+    parts = cmd.split()
+    if parts[0] == "game":
+        webbrowser.open(f'https://retroachievements.org/game/{parts[1]}')
+    else:
+        print("invalid command")
+
+
+def interpreter(cmd):
+    if cmd == "exit":
+        exit()
+    elif cmd == "help":
+        printHelp()
+    elif cmd == "list":
+        printList()
+    elif cmd == "check":
+        openChecklistWebPage()
+    elif cmd.find("game") != -1:
+        openGameWebPage(cmd)
+    else:
+        print("invalid command")
+
 
 # Main loop
 while True:
-    print("0: exit\n1: read id-list.json\n")
-
-    option = input()
-    if option == '0':
-        exit()
-    elif option == '1':
-        printList()
-    else:
-        pass
+    interpreter(input())
+    print("")
